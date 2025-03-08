6b8dec5dbbe764bee75242b9eb1f70cc4a669fcc
Jessi Jaramillo <jaramij2@sunypoly.edu>
2025-03-07 20:21:39 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ udash = pd.read_csv('university_student_dashboard_data.csv')
 col1, col2 = st.columns([1, 4])  # Adjust column widths as needed
 
 with col1: #Logo
-    st.image("SUNY-Poly-vertical-logo.png", width=80)
+    st.image("SUNY-Poly-vertical-logo.png", width=200)
 
 with col2: # Title
     st.title("Student Admissions Dashboard")
